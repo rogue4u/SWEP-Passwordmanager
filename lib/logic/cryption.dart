7dc9337c52b95f99a8f62beae7486583989d5d79
Fenex <31255152+thumpnail@users.noreply.github.com>
2023-05-03 17:52:26 +0200

    Update cryption.dart
    Added Encryption functions

--- lib/logic/cryption.dart
+++ lib/logic/cryption.dart
@@ -1,18 +1,31 @@
 import 'dart:convert';
+import 'dart:ffi';
 import 'package:encrypt/encrypt.dart';
 
-String encryptAES(String plainText, String _key){
-  final key = Key.fromUtf8(_key);
-  final encrypter = Encrypter(AES(key));
-  final encrypted = encrypter.encrypt(plainText);
+class Decryption {
+  static final iv = IV.fromBase64('FxIOBAcEEhISHgICCRYhEA==');
 
-  return encrypted.base64;
-}
+  static String encryptAES(String plainText, String _key) {
+    final key = Key.fromUtf8(to256(_key, 32));
+    final encrypter = Encrypter(AES(key, mode: AESMode.sic, padding: null));
+    final encrypted = encrypter.encrypt(plainText, iv: iv);
 
-String decryptAES(String plainText, String _key){
-  final key = Key.fromUtf8(_key);
-  final encrypter = Encrypter(AES(key));
-  final decrypted = encrypter.decrypt16(plainText);
+    return encrypted.base64;
+  }
 
-  return decrypted;
+  static String decryptAES(String plainText, String _key) {
+    final key = Key.fromUtf8(to256(_key, 32));
+    final encrypter = Encrypter(AES(key, mode: AESMode.sic, padding: null));
+    final decrypted = encrypter.decrypt(Encrypted.fromBase64(plainText), iv: iv);
+
+    return decrypted;
+  }
+
+  static String to256(String input, num length) {
+    var res = "";
+    for (var i = 0; i < length; i++) {
+      res += input[i % input.length];
+    }
+    return res;
+  }
 }
